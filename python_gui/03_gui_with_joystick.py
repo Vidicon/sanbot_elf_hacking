a79ed8e4c4b3387024df49d85bdda34e12fc6eb8
matthijsfh <matthijsfh@planet.nl>
2024-11-11 19:06:03 +0100

    Updated layout. Added all distance sensors.

--- python_gui/03_gui_with_joystick.py
+++ python_gui/03_gui_with_joystick.py
@@ -92,8 +92,8 @@ def my_receive_callback(data, stream_area):
     if (response == (CMD_VERSION | RESP_BIT)):
         
         string_from_bytearray = data[3:-2].decode('utf-8') 
-        text_area.insert(tk.END, "< " + string_from_bytearray + "\n")
-        text_area.yview_moveto(1)  # Scrolling to the bottom        
+        stream_area.insert(tk.END, "< " + string_from_bytearray + "\n")
+        stream_area.yview_moveto(1)  # Scrolling to the bottom        
     
     
     if (response == (CMD_GET_ENCODERS | RESP_BIT)):
@@ -145,6 +145,22 @@ def my_receive_callback(data, stream_area):
         combined4_int = int.from_bytes(new_byte_array[6:8], byteorder='big')  # 'big' for big-endian, 'little' for little-endian
         SetDistanceButtonBGColor(distance4_Front, combined4_int)
 
+        # --------------------------------------------------------------------------
+        combined5_int = int.from_bytes(new_byte_array[8:10], byteorder='big')  # 'big' for big-endian, 'little' for little-endian
+        SetDistanceButtonBGColor(distance5_Front, combined5_int)
+        
+        # --------------------------------------------------------------------------
+        combined6_int = int.from_bytes(new_byte_array[10:12], byteorder='big')  # 'big' for big-endian, 'little' for little-endian
+        SetDistanceButtonBGColor(distance6_Front, combined6_int)
+
+        # --------------------------------------------------------------------------
+        combined7_int = int.from_bytes(new_byte_array[12:14], byteorder='big')  # 'big' for big-endian, 'little' for little-endian
+        SetDistanceButtonBGColor(distance7_Front, combined7_int)
+
+        # --------------------------------------------------------------------------
+        combined8_int = int.from_bytes(new_byte_array[14:16], byteorder='big')  # 'big' for big-endian, 'little' for little-endian
+        SetDistanceButtonBGColor(distance8_Front, combined8_int)
+
     
 def createCommand(mod_manager, InputCommmand, Parameters):
     
@@ -192,7 +208,7 @@ def createDoubleMoveCommand(mod_manager, Parameters):
 # Clear logging
 #===============================================================================
 def clear_logging():
-    text_area.delete('1.0', 'end')  # Deletes from start to end
+    # text_area.delete('1.0', 'end')  # Deletes from start to end
     stream_area.delete('1.0', 'end')  # Deletes from start to end
     return
 
@@ -226,14 +242,6 @@ def show_gui(mod_manager):
     #--------------------------------------------------------------------------------------
     # Left arm
     #--------------------------------------------------------------------------------------
-    global button_version
-    button_version = tk.Button(frame, 
-                               height= 1, 
-                               width=10, 
-                               text="Get Version", 
-                               command=lambda t=[0]: createCommand(mod_manager, CMD_VERSION, t))
-    button_version.grid(row=0, column=0, sticky="w")
-
     global button_debug1
     button_debug1 = tk.Button(frame, 
                                height= 1, 
@@ -250,36 +258,33 @@ def show_gui(mod_manager):
                                command=lambda t=np.array([CMD_LA_MOVE, -150, CMD_RA_MOVE, 150]): createDoubleMoveCommand(mod_manager, t))
     button_debug2.grid(row=1, column=7, sticky="w")
 
-    global default_bg 
-    default_bg = button_version.cget('bg')
-
     button_LA_White = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Left White", 
                               command=lambda t=np.array([CMD_LA_COLOR, CMD_WHITE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_LA_White.grid(row=1, column=0, sticky="w")
+    button_LA_White.grid(row=0, column=0, sticky="w")
 
     button_LA_Red = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Left Red", 
                               command=lambda t=np.array([CMD_LA_COLOR, CMD_RED, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_LA_Red.grid(row=2, column=0, sticky="w")
+    button_LA_Red.grid(row=1, column=0, sticky="w")
 
     button_LA_Green = tk.Button(frame, 
                                 height= 1, 
                                 width=10, 
                                 text="Left Green", 
                                 command=lambda t=np.array([CMD_LA_COLOR, CMD_GREEN, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_LA_Green.grid(row=3, column=0, sticky="w")
+    button_LA_Green.grid(row=2, column=0, sticky="w")
 
     button_LA_Blue = tk.Button(frame, 
                                height= 1, 
                                width=10, 
                                text="Left Blue", 
                                command=lambda t=np.array([CMD_LA_COLOR, CMD_BLUE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_LA_Blue.grid(row=4, column=0, sticky="w")
+    button_LA_Blue.grid(row=3, column=0, sticky="w")
 
 
     button_LA_Off = tk.Button(frame, 
@@ -287,7 +292,7 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Left Off", 
                                command=lambda t=np.array([CMD_LA_COLOR, CMD_ALL, CMD_LED_OFF]): createLedCommand(mod_manager, t))
-    button_LA_Off.grid(row=5, column=0, sticky="w")
+    button_LA_Off.grid(row=4, column=0, sticky="w")
 
 
     button_LA_Blue_Blink = tk.Button(frame, 
@@ -295,7 +300,20 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Left Blue Blink", 
                                command=lambda t=np.array([CMD_LA_COLOR, CMD_BLUE, CMD_LED_BLINK_VERYFAST]): createLedCommand(mod_manager, t))
-    button_LA_Blue_Blink.grid(row=6, column=0, sticky="w")
+    button_LA_Blue_Blink.grid(row=5, column=0, sticky="w")
+
+
+    global button_version
+    button_version = tk.Button(frame, 
+                               height= 1, 
+                               width=10, 
+                               text="Get Version", 
+                               command=lambda t=[0]: createCommand(mod_manager, CMD_VERSION, t))
+    button_version.grid(row=8, column=0, sticky="w")
+
+    global default_bg 
+    default_bg = button_version.cget('bg')
+
 
     #--------------------------------------------------------------------------------------
     # Right arm
@@ -305,28 +323,28 @@ def show_gui(mod_manager):
                               width=10, 
                               text="Right White", 
                               command=lambda t=np.array([CMD_RA_COLOR, CMD_WHITE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_RA_White.grid(row=1, column=1, sticky="w")
+    button_RA_White.grid(row=0, column=1, sticky="w")
 
     button_RA_Red = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Right Red", 
                               command=lambda t=np.array([CMD_RA_COLOR, CMD_RED, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_RA_Red.grid(row=2, column=1, sticky="w")
+    button_RA_Red.grid(row=1, column=1, sticky="w")
 
     button_RA_Green = tk.Button(frame, 
                                 height= 1, 
                                 width=10, 
                                 text="Right Green", 
                                 command=lambda t=np.array([CMD_RA_COLOR, CMD_GREEN, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_RA_Green.grid(row=3, column=1, sticky="w")
+    button_RA_Green.grid(row=2, column=1, sticky="w")
 
     button_RA_Blue = tk.Button(frame, 
                                height= 1, 
                                width=10, 
                                text="Right Blue", 
                                command=lambda t=np.array([CMD_RA_COLOR, CMD_BLUE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_RA_Blue.grid(row=4, column=1, sticky="w")
+    button_RA_Blue.grid(row=3, column=1, sticky="w")
 
 
     button_RA_Off = tk.Button(frame, 
@@ -334,7 +352,7 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Right Off", 
                                command=lambda t=np.array([CMD_RA_COLOR, CMD_ALL, CMD_LED_OFF]): createLedCommand(mod_manager, t))
-    button_RA_Off.grid(row=5, column=1, sticky="w")
+    button_RA_Off.grid(row=4, column=1, sticky="w")
 
 
     button_RA_Blue_Blink = tk.Button(frame, 
@@ -342,7 +360,7 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Right Blue Blink", 
                                command=lambda t=np.array([CMD_RA_COLOR, CMD_BLUE, CMD_LED_BLINK_VERYFAST]): createLedCommand(mod_manager, t))
-    button_RA_Blue_Blink.grid(row=6, column=1, sticky="w")
+    button_RA_Blue_Blink.grid(row=5, column=1, sticky="w")
 
     #--------------------------------------------------------------------------------------
     # Right arm
@@ -352,28 +370,28 @@ def show_gui(mod_manager):
                               width=10, 
                               text="Base White", 
                               command=lambda t=np.array([CMD_BASE_COLOR, CMD_WHITE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_Base_Off.grid(row=1, column=2, sticky="w")
+    button_Base_Off.grid(row=0, column=2, sticky="w")
 
     button_Base_Red = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Base Red", 
                               command=lambda t=np.array([CMD_BASE_COLOR, CMD_RED, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_Base_Red.grid(row=2, column=2, sticky="w")
+    button_Base_Red.grid(row=1, column=2, sticky="w")
 
     button_Base_Green = tk.Button(frame, 
                                 height= 1, 
                                 width=10, 
                                 text="Base Green", 
                                 command=lambda t=np.array([CMD_BASE_COLOR, CMD_GREEN, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_Base_Green.grid(row=3, column=2, sticky="w")
+    button_Base_Green.grid(row=2, column=2, sticky="w")
 
     button_Base_Blue = tk.Button(frame, 
                                height= 1, 
                                width=10, 
                                text="Base Blue", 
                                command=lambda t=np.array([CMD_BASE_COLOR, CMD_BLUE, CMD_LED_ON]): createLedCommand(mod_manager, t))
-    button_Base_Blue.grid(row=4, column=2, sticky="w")
+    button_Base_Blue.grid(row=3, column=2, sticky="w")
 
 
     button_Base_Blue = tk.Button(frame, 
@@ -381,7 +399,7 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Base Off", 
                                command=lambda t=np.array([CMD_BASE_COLOR, CMD_ALL, CMD_LED_OFF]): createLedCommand(mod_manager, t))
-    button_Base_Blue.grid(row=5, column=2, sticky="w")
+    button_Base_Blue.grid(row=4, column=2, sticky="w")
 
 
     button_Base_Blue_Blink = tk.Button(frame, 
@@ -389,7 +407,7 @@ def show_gui(mod_manager):
                                width=10, 
                                text="Base Blue Blink", 
                                command=lambda t=np.array([CMD_BASE_COLOR, CMD_BLUE, CMD_LED_BLINK_VERYFAST]): createLedCommand(mod_manager, t))
-    button_Base_Blue_Blink.grid(row=6, column=2, sticky="w")
+    button_Base_Blue_Blink.grid(row=5, column=2, sticky="w")
 
     #--------------------------------------------------------------------------------------
     # Move Left arm
@@ -399,21 +417,21 @@ def show_gui(mod_manager):
                               width=10, 
                               text="Left Move 1", 
                               command=lambda t=np.array([CMD_LA_MOVE, -500]): createMoveCommand(mod_manager, t))
-    button_LA_Move_1.grid(row=1, column=3, sticky="w")
+    button_LA_Move_1.grid(row=0, column=3, sticky="w")
 
     button_LA_Move_2 = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Left Move 2", 
                               command=lambda t=np.array([CMD_LA_MOVE, -350]): createMoveCommand(mod_manager, t))
-    button_LA_Move_2.grid(row=2, column=3, sticky="w")
+    button_LA_Move_2.grid(row=1, column=3, sticky="w")
 
     button_LA_Move_3 = tk.Button(frame, 
                                 height= 1, 
                                 width=10, 
                                 text="Left  Move 3", 
                                 command=lambda t=np.array([CMD_LA_MOVE, -150]): createMoveCommand(mod_manager, t))
-    button_LA_Move_3.grid(row=3, column=3, sticky="w")
+    button_LA_Move_3.grid(row=2, column=3, sticky="w")
 
     #--------------------------------------------------------------------------------------
     # Move Right arm
@@ -423,21 +441,21 @@ def show_gui(mod_manager):
                               width=10, 
                               text="Right Move 1", 
                               command=lambda t=np.array([CMD_RA_MOVE, 500]): createMoveCommand(mod_manager, t))
-    button_RA_Move_1.grid(row=1, column=4, sticky="w")
+    button_RA_Move_1.grid(row=0, column=4, sticky="w")
 
     button_RA_Move_2 = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Right Move 2", 
                               command=lambda t=np.array([CMD_RA_MOVE, 350]): createMoveCommand(mod_manager, t))
-    button_RA_Move_2.grid(row=2, column=4, sticky="w")
+    button_RA_Move_2.grid(row=1, column=4, sticky="w")
 
     button_RA_Move_3 = tk.Button(frame, 
                                 height= 1, 
                                 width=10, 
                                 text="Right  Move 3", 
                                 command=lambda t=np.array([CMD_RA_MOVE, 150]): createMoveCommand(mod_manager, t))
-    button_RA_Move_3.grid(row=3, column=4, sticky="w")
+    button_RA_Move_3.grid(row=2, column=4, sticky="w")
     
     #--------------------------------------------------------------------------------------
     # Motion sensors front & back
@@ -447,43 +465,73 @@ def show_gui(mod_manager):
                               height= 1, 
                               width=10, 
                               text="Motion Front")
-    button_Motion_Front.grid(row=1, column=5, sticky="w")
+    button_Motion_Front.grid(row=0, column=5, sticky="w")
 
     global button_Motion_Back
     button_Motion_Back = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="Motion Back")
-    button_Motion_Back.grid(row=2, column=5, sticky="w")
+    button_Motion_Back.grid(row=1, column=5, sticky="w")
 
-
+    #--------------------------------------------------------------------------------------
+    # Distance sensors
+    #--------------------------------------------------------------------------------------
     global distance1_Front
     distance1_Front = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="inf")
-    distance1_Front.grid(row=1, column=6, sticky="w")
+    distance1_Front.grid(row=0, column=6, sticky="w")
 
     global distance2_Front
     distance2_Front = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="inf")
-    distance2_Front.grid(row=2, column=6, sticky="w")
+    distance2_Front.grid(row=1, column=6, sticky="w")
 
     global distance3_Front
     distance3_Front = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="inf")
-    distance3_Front.grid(row=3, column=6, sticky="w")
+    distance3_Front.grid(row=2, column=6, sticky="w")
 
     global distance4_Front
     distance4_Front = tk.Button(frame, 
                               height= 1, 
                               width=10, 
                               text="inf")
-    distance4_Front.grid(row=4, column=6, sticky="w")
+    distance4_Front.grid(row=3, column=6, sticky="w")
+
+    global distance5_Front
+    distance5_Front = tk.Button(frame, 
+                              height= 1, 
+                              width=10, 
+                              text="inf")
+    distance5_Front.grid(row=4, column=6, sticky="w")
+
+    global distance6_Front
+    distance6_Front = tk.Button(frame, 
+                              height= 1, 
+                              width=10, 
+                              text="inf")
+    distance6_Front.grid(row=5, column=6, sticky="w")
+
+    global distance7_Front
+    distance7_Front = tk.Button(frame, 
+                              height= 1, 
+                              width=10, 
+                              text="inf")
+    distance7_Front.grid(row=6, column=6, sticky="w")
+
+    global distance8_Front
+    distance8_Front = tk.Button(frame, 
+                              height= 1, 
+                              width=10, 
+                              text="inf")
+    distance8_Front.grid(row=7, column=6, sticky="w")
 
     #--------------------------------------------------------------------------------------
     # Generic buttons
@@ -491,11 +539,11 @@ def show_gui(mod_manager):
     button_clear = tk.Button(frame, height= 1, text="Clear", command=lambda t="\"failed\"": clear_logging())
     button_clear.grid(row=11, column=0, columnspan=1, sticky="w")
 
-    #--------------------------------------------------------------------------------------
-    # event data area
-    #--------------------------------------------------------------------------------------
-    text_area = scrolledtext.ScrolledText(frame, width = 200, height=20, wrap="word")
-    text_area.grid(row=9, column=0, columnspan=10, sticky="w")
+    # #--------------------------------------------------------------------------------------
+    # # event data area
+    # #--------------------------------------------------------------------------------------
+    # text_area = scrolledtext.ScrolledText(frame, width = 200, height=20, wrap="word")
+    # text_area.grid(row=9, column=0, columnspan=10, sticky="w")
 
     #--------------------------------------------------------------------------------------
     # streaming data area
@@ -503,7 +551,7 @@ def show_gui(mod_manager):
     stream_area = scrolledtext.ScrolledText(frame, width = 200, height=20, wrap="word")
     stream_area.grid(row=10, column=0, columnspan=10, sticky="w")
 
-    return root, text_area, stream_area
+    return root, stream_area
 
 # Function to handle pygame events
 def handle_pygame_events():
@@ -656,7 +704,7 @@ def main():
     # mod_manager = ModManager(port='/dev/ttyACM0', baudrate=115200)
     global mod_manager 
     global root
-    global text_area
+    # global text_area
     global stream_area
 
    
@@ -666,7 +714,7 @@ def main():
     else:
         mod_manager = ModManager(port='COM9', baudrate=115200)
         
-    root, text_area, stream_area = show_gui(mod_manager)
+    root, stream_area = show_gui(mod_manager)
 
     # Set the receive callback
     mod_manager.open_port()
