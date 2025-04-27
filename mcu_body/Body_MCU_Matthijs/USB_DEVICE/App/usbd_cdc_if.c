d964638794cb27caff94306f50a67bcaa590a166
matthijsfh <matthijsfh@planet.nl>
2025-04-27 13:00:57 +0200

    Repaired com to body

--- mcu_body/Body_MCU_Matthijs/USB_DEVICE/App/usbd_cdc_if.c
+++ mcu_body/Body_MCU_Matthijs/USB_DEVICE/App/usbd_cdc_if.c
@@ -22,6 +22,7 @@
 #include "usbd_cdc_if.h"
 
 /* USER CODE BEGIN INCLUDE */
+#include "protocol_0x55.h"
 
 /* USER CODE END INCLUDE */
 
@@ -259,6 +260,9 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+
+	Protocol_0x55_NewData(Buf, Len);
+
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
